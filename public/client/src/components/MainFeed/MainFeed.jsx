cacfe5d77787c212b0543bd0af22b41d020588ec
choieuicheon <choieuicheon@naver.com>
2023-01-13 22:56:04 +0900

    23.01.13

--- public/client/src/components/MainFeed/MainFeed.jsx
+++ public/client/src/components/MainFeed/MainFeed.jsx
@@ -8,7 +8,7 @@ function MainFeed() {
             <div className="feed-list">
                 <div className="write-feed">
 
-                    <div classnmae="profile-image"></div>
+                    <div className="profile-image"></div>
                     <div className="inp">
                         <input type="text" placeholder="오늘의 일상"/>
                     </div>
@@ -21,10 +21,187 @@ function MainFeed() {
                     </div>
                     
                 </div>
+
+
+                <div className="feed">
+                    <div className="top">
+                        <div className="profile-image">
+
+                        </div>
+                        <div className="profile-desc">
+                            <div className="nickname">
+                                choieuicheon
+                            </div>
+                            <div className="timestamp">
+                                8:15 pm, yesterday
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contents">
+                        Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Reprehenderit molestias qui ipsa quia libero, 
+                        pariatur debitis nihil unde quam minima voluptatem soluta nemo ab, 
+                        repellat consequatur labore alias, velit aliquid.
+                    </div>
+                    <div className="bottom">
+                        <div className="like">
+                            <div className="assets">
+                                <img src="/assets/feed/like-dac.svg" alt="좋아요"/>
+                            </div>
+                            <div className="count txt-bold">25k</div>
+                        </div>
+                        <div className="comment">
+                            <div className="assets">
+                                <img src="/assets/feed/comment.svg" alt="댓글"/></div>
+                            <div className="count txt-bold">5k</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="feed">
+                    <div className="top">
+                        <div className="profile-image">
+
+                        </div>
+                        <div className="profile-desc">
+                            <div className="nickname">
+                                choieuicheon
+                            </div>
+                            <div className="timestamp">
+                                8:15 pm, yesterday
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contents">
+                        Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Reprehenderit molestias qui ipsa quia libero, 
+                        pariatur debitis nihil unde quam minima voluptatem soluta nemo ab, 
+                        repellat consequatur labore alias, velit aliquid.
+                        <div className="image">
+
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="like">
+                            <div className="assets">
+                                <img src="/assets/feed/like-dac.svg" alt="좋아요"/>
+                            </div>
+                            <div className="count txt-bold">25k</div>
+                        </div>
+                        <div className="comment">
+                            <div className="assets">
+                                <img src="/assets/feed/comment.svg" alt="댓글"/></div>
+                            <div className="count txt-bold">5k</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="feed">
+                    <div className="top">
+                        <div className="profile-image">
+
+                        </div>
+                        <div className="profile-desc">
+                            <div className="nickname">
+                                choieuicheon
+                            </div>
+                            <div className="timestamp">
+                                8:15 pm, yesterday
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contents">
+                        Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Reprehenderit molestias qui ipsa quia libero, 
+                        pariatur debitis nihil unde quam minima voluptatem soluta nemo ab, 
+                        repellat consequatur labore alias, velit aliquid.
+                    </div>
+                    <div className="bottom">
+                        <div className="like">
+                            <div className="assets">
+                                <img src="/assets/feed/like-dac.svg" alt="좋아요"/>
+                            </div>
+                            <div className="count txt-bold">25k</div>
+                        </div>
+                        <div className="comment">
+                            <div className="assets">
+                                <img src="/assets/feed/comment.svg" alt="댓글"/></div>
+                            <div className="count txt-bold">5k</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="feed">
+                    <div className="top">
+                        <div className="profile-image">
+
+                        </div>
+                        <div className="profile-desc">
+                            <div className="nickname">
+                                choieuicheon
+                            </div>
+                            <div className="timestamp">
+                                8:15 pm, yesterday
+                            </div>
+                        </div>
+                    </div>
+                    <div className="contents">
+                        Lorem ipsum dolor sit amet consectetur, 
+                        adipisicing elit. Reprehenderit molestias qui ipsa quia libero, 
+                        pariatur debitis nihil unde quam minima voluptatem soluta nemo ab, 
+                        repellat consequatur labore alias, velit aliquid.
+                        <div className="image">
+                            
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="like">
+                            <div className="assets">
+                                <img src="/assets/feed/like-dac.svg" alt="좋아요"/>
+                            </div>
+                            <div className="count txt-bold">25k</div>
+                        </div>
+                        <div className="comment">
+                            <div className="assets">
+                                <img src="/assets/feed/comment.svg" alt="댓글"/></div>
+                            <div className="count txt-bold">5k</div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
             <div className="friend-list">
-
+                <div className="my-profile">
+                    <div className="profile-image"></div>
+                    <div className="nickname txt-bold">choieuicheon</div>
+                </div>
+                <div className="my-friends">
+                    <div className="title txt-bold">friends</div>
+                    <ul className="friend-list-wrapper">
+                        <li className="friend">
+                            <div className="profile-image"></div>
+                            <div className="nickname txt-bold">nickname-1</div>
+                        </li>
+                        <li className="friend">
+                            <div className="profile-image"></div>
+                            <div className="nickname txt-bold">nickname-2</div>
+                        </li>
+                        <li className="friend">
+                            <div className="profile-image"></div>
+                            <div className="nickname txt-bold">nickname-3</div>
+                        </li>
+                        <li className="friend">
+                            <div className="profile-image"></div>
+                            <div className="nickname txt-bold">nickname-4</div>
+                        </li>
+                        <li className="friend">
+                            <div className="profile-image"></div>
+                            <div className="nickname txt-bold">nickname-5</div>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
         </div>
